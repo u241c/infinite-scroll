5ee21b5fd8ba92eb8c921e9f7cd3ed80928c2678
u241c <jr4701.fsj@gmail.com>
2021-07-24 13:04:32 -0500

    Wip: Image loaded has been declared

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imageLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'TGDFcjm__wBxFIcDiPFCQxwfDul5ktD71GyFpC_I8qQ';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  console.log('image loaded');
+  imagesLoaded++;
+  if (imagesLoaded === totalImages){
+    ready = true;
+    console.log('ready =', ready);
+  }
+}
 
 // Helper function to set Attributes on the DOM Elements
 function setAttributes(element, attributes){
@@ -17,6 +30,8 @@ function setAttributes(element, attributes){
 
 // Create Elements for links & Photos, add to Dom
 function displayPhotos(){
+  totalImages = photosArray.length;
+  console.log('total images', totalImages);
   //Run function for each object in photosArray
   photosArray.forEach((photo) => {
   // Create <a> to link to unsplash
@@ -32,6 +47,9 @@ function displayPhotos(){
     alt: photo.alt_description,
     title: photo.alt_description,
   });
+  // Event Listener, check when each is finished loading
+  img.addEventListener('load', imageLoaded);
+
   // put <img> inside <a>, then put both inside imageContainer Element
   item.appendChild(img);
   imageContainer.appendChild(item);
